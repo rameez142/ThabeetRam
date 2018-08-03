2a04803012c8d99936765139d4c9157f499865c8
Mohd Imran <mohdimran043@gmail.com>
2018-08-03 17:12:21 +0300

    inital checkin

---
+++ src/QuickApp/ClientApp/src/app/components/controls/banner-demo.component.ts
@@ -0,0 +1,19 @@
+// ====================================================
+// More Templates: https://www.ebenmonney.com/templates
+// Email: support@ebenmonney.com
+// ====================================================
+
+import { Component } from '@angular/core';
+
+
+
+@Component({
+    selector: 'banner-demo',
+    templateUrl: './banner-demo.component.html'
+})
+export class BannerDemoComponent {
+    banner1 = require("../../assets/images/demo/doha1.jpg");
+  banner2 = require("../../assets/images/demo/doha2.jpg");
+  banner3 = require("../../assets/images/demo/doha3.jpg");
+  banner4 = require("../../assets/images/demo/doha4.jpg");
+}
---
+++ src/QuickApp/ClientApp/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+// ====================================================
+// More Templates: https://www.ebenmonney.com/templates
+// Email: support@ebenmonney.com
+// ====================================================
+
+export const environment = {
+  production: true,
+  baseUrl: 'http://localhost:2020', //Change this to the address of your backend API if different from frontend address
+  loginUrl: "/Login",
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA'
+  }
+};
