6c99cdbc16e0a9381b53126156fab11cf3f407a5
Mohd Imran <mohdimran043@gmail.com>
2018-08-05 10:46:59 +0300

    Checking in the fix

--- src/QuickApp/ClientApp/src/environments/environment.prod.ts
+++ src/QuickApp/ClientApp/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:2020', //Change this to the address of your backend API if different from frontend address
+  baseUrl: null, //Change this to the address of your backend API if different from frontend address
   loginUrl: "/Login",
   mapbox: {
     accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA'
