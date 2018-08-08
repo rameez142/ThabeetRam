7d4eb68e10c36f64d240d0e5449007500f21bf9c
Rameez <rameez.rameez14@gmail.com>
2018-08-08 07:20:50 +0300

    Sidenav Changes

--- src/QuickApp/ClientApp/src/app/services/sidenav.service.ts
+++ src/QuickApp/ClientApp/src/app/services/sidenav.service.ts
@@ -5,60 +5,107 @@ export class Product {
     text: string;
     expanded?: boolean;
     selected?: boolean;
-    price?: number;
     items?: Product[];
 }
 
-var products: Product[] = [{
+var products: Product[] = [
+  {
     id: "1_1_1",
-    text: "Video Players",
+  text: "All Vehicles",
     items: [{
         id: "1_1_1_1",
-        text: "HD Video Player",
-        price: 220
+      text: "26470"
     }, {
         id: "1_1_1_2",
-        text: "SuperHD Video Player",
-        price: 270
+        text: "283584"
     }]
 }, {
     id: "1_1_2",
-    text: "Televisions",
-    expanded: true,
+    text: "Moving Vehicles",
     items: [{
-        id: "1_1_2_1",
-        text: "SuperLCD 42",
-        price: 1200
+      id: "1_1_2_1",
+      text: "26570"
     }, {
-        id: "1_1_2_2",
-        text: "SuperLED 42",
-        price: 1450
-    }, {
-        id: "1_1_2_3",
-        text: "SuperLED 50",
-        price: 1600
-    }, {
-        id: "1_1_2_4",
-        text: "SuperLCD 55",
-        price: 1350
-    }, {
-        id: "1_1_2_5",
-        text: "SuperLCD 70",
-        price: 4000
+      id: "1_1_2_2",
+      text: "26670"
     }]
 }, {
+    id: "1_1_3",
+    text: "Stopping Vehicles",
+    items: [{
+        id: "1_1_3_1",
+        text: "26870"
+      }]
+}, {
     id: "1_1_4",
-    text: "Projectors",
+    text: "GPS Communication Problem",
     items: [{
         id: "1_1_4_1",
-        text: "Projector Plus",
-        price: 550
-    }, {
-        id: "1_1_4_2",
-        text: "Projector PlusHD",
-        price: 750
-    }]
-}];
+        text: "26870"
+      }]
+}, {
+    id: "1_1_5",
+    text: "Vehicles That Have Sent Alarm",
+    items: [{
+        id: "1_1_5_1",
+        text: "26870"
+      }]
+}
+, {
+    id: "1_1_6",
+    text: "Vehicles That Cannot Receive GPS Signal",
+    items: [{
+        id: "1_1_6_1",
+        text: "26890"
+      }]
+}, {
+    id: "1_1_7",
+    text: "Routes",
+    items: [{
+        id: "1_1_7_1",
+        text: "المنهج 22/06/2015 09:37:37 "
+      }]
+}, {
+    id: "1_1_8",
+    text: "(60) ادارة",
+    items: [{
+        id: "1_8_1",
+        text: "(23) قسم"
+      },
+      {
+        id: "1_8_2",
+        text: "(28) قسم"
+      }]
+}, {
+    id: "1_1_9",
+    text: "الدوريات 88",
+    items: [{
+        id: "1_9_1",
+        text: "(23) قسم"
+      }]
+}, {
+    id: "1_1_10",
+    text: "الفريق الخاص",
+    items: [{
+        id: "1_10_1",
+        text: "(23) قسم"
+      }]
+}, {
+    id: "1_1_11",
+    text: "دخان",
+    items: [{
+        id: "1_11_1",
+        text: "(23) قسم"
+      }]
+}, {
+    id: "1_1_12",
+    text: "مطار حمد الدولي",
+    items: [{
+        id: "1_12_1",
+        text: "(23) قسم"
+      }]
+}
+];
 
 @Injectable()
 export class Service {
